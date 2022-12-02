7c4efaf36356e0d64b789cb9e7ed52acc7afcdd1
Tomas Balaz <balaztomas0@gmail.com>
2022-12-02 13:33:13 +0100

    Created spring-boot-3-introduction application

---
+++ src/test/java/com/example/springboot3introduction/SpringBoot3IntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3IntroductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
